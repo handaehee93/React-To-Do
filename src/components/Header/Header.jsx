ddedf3c5bc3e44df8401e555521057fe0dd73bdc
handaehee93 <hdh8990@naver.com>
2023-04-18 15:17:07 +0900

    필터링 구현

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Header({ filters, filter, onFilterChange }) {
+  return (
+    <header>
+      <ul>
+        {filters.map((value, index) => (
+          <li key={index}>
+            <button onClick={() => onFilterChange(value)}>{value}</button>
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
+}
